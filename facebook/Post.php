403cfc861775c9812f3778cc06720ff3733dfab6
romaindeuxtrois <romain.mschn@eduge.ch>
2020-02-27 11:34:20 +0100

    27.02.2020

--- facebook/Post.php
+++ facebook/Post.php
@@ -2,25 +2,31 @@
 <?php
 var_dump($_FILES);
 for ($i = 0; $i < count($_FILES['image']['name']); $i++) {
-     echo 'asd';
+
      $date = date('Y-m-d H:i:s');
      $commentaire = filter_input(INPUT_POST, "commentaire", FILTER_SANITIZE_STRING);
      $dossier = 'upload/';
-     $fichier = basename($_FILES['image']['tmp_name']);
+     $fichier = basename($_FILES['image']['tmp_name'][$i]);
      $taille_maxi = 70000000;
      $taille = filesize($_FILES['image']['tmp_name'][$i]);
      $extensions = array('.png', '.gif', '.jpg', '.jpeg');
-     $extension = strrchr($_FILES['image']['name'], '.');
+     $extension = strrchr($_FILES['image']['name'][$i], '.');
      $fichier = $fichier . (new DateTime())->getTimestamp() . $extension;
-     
-          if (move_uploaded_file($_FILES['image']['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
-          {
-               echo 'Upload effectué avec succès !';
-               header("Location: index.html");
-          } else //Sinon (la fonction renvoie FALSE).
-          {
-               echo 'Echec de l\'upload !';
-          }
+
+
+     $allowed = array('gif', 'png', 'jpg');
+     $filename = $_FILES['image']['name'][$i];
+     $ext = pathinfo($filename, PATHINFO_EXTENSION);
+     if (!in_array($ext, $allowed)) {
+          echo 'error not an image';
+     }else if (move_uploaded_file($_FILES['image']['tmp_name'][$i], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+     {
+          echo 'Upload effectué avec succès !';
+          //header("Location: index.html");
+     } else //Sinon (la fonction renvoie FALSE).
+     {
+          echo 'Echec de l\'upload !';
+     }
 }
 
 
@@ -34,7 +40,7 @@ for ($i = 0; $i < count($_FILES['image']['name']); $i++) {
 
      <form method="POST" action="#" enctype="multipart/form-data">
 
-          Fichier : <input type="file" multiple accept="image/*" name="image">
+          Fichier : <input type="file" multiple accept="image/*" name="image[]">
           Commentaire : <input type="textarea" name="commentaire">
           <input type="submit" name="envoyer" value="Envoyer le fichier">
 
